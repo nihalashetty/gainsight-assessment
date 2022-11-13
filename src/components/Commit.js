1590b2ede621dcdaffa2e2dbd6934243037459e6
nihalashetty <nihalsetty48@gmail.com>
2022-11-13 22:37:58 +0530

    Final Commit Redaing key from localstorage if present and error handling

--- src/components/Commit.js
+++ src/components/Commit.js
@@ -5,7 +5,7 @@ export default function Commit (props) {
         <div className='commit' key={props.index}>
             <div className='commit-info'>
                 <h4 className="committed-messahe">{props.data.commit.message}</h4>
-                <p>Committed by <bold>{props.data.commit.committer.name}</bold> on <bold>{props.data.commit.committer.date}</bold></p>
+                <p>Committed by <b>{props.data.commit.committer.name}</b> on <b>{props.data.commit.committer.date}</b></p>
             </div>
             <div className='commit-link'>
                 <a href={props.data.html_url} className='commit-url'>More Details</a>
--- src/components/GitCommits.js
+++ src/components/GitCommits.js
@@ -3,16 +3,31 @@ import Commit from "./Commit";
 import { Octokit } from "octokit"
 
 export default function GitCommits() {
-    const [key, setKey] = React.useState('');
+    const [key, setKey] = React.useState(localStorage.getItem('publicKey') || '');
     const [commits, setCommits] = React.useState([]);
     const [time, setTime] = React.useState(30);
     const [isTimerActive, setIsTimerActive] = React.useState(false);
+    const [invalidKey, setInvalidKey] = React.useState(false);
 
+    //Check if key was present on localstorage and get commits on load
+    React.useEffect(() => {
+        if (key) {
+            getCommits();
+        }
+    }, []);
+
+    //Set Key to localstorage on change
+    React.useEffect(() => {
+        if (key) {
+            localStorage.setItem('publicKey', key)
+        }
+    }, [key]);
+
+    //Set timer and run for every 30Sec ones data is fetched
     React.useEffect(() => {
         let interval = null;
         if (isTimerActive) {
             interval = setInterval(() => {
-                console.log(time)
                 setTime((time) => {
                     if (time === 0) {
                         getCommits();
@@ -32,31 +47,41 @@ export default function GitCommits() {
     }, [isTimerActive]);
 
     const changeHandle = (e) => {
+        setTime(30);
         setKey(e.target.value);
         setIsTimerActive(false);
     }
 
     const submitHandle = () => {
-        getCommits();
-        setIsTimerActive(true);
+        if (key) {
+            setTime(30);
+            getCommits();
+        }
     }
 
     const refreshHandle = () => {
-        setTime(0);
+        setTime(30);
         getCommits();
     }
 
     const getCommits = async () => {
+        //{key} was throwing not a string exception, hence using `${key}`
         const octokit = new Octokit({
             auth: `${key}`
         });
 
-        const response = await octokit.request('GET /repos/{owner}/{repo}/commits{?sha,path,author,since,until,per_page,page}', {
-            owner: 'nihalashetty',
-            repo: 'gainsight-assessment'
-        });
+        try {
+            const response = await octokit.request('GET /repos/{owner}/{repo}/commits{?sha,path,author,since,until,per_page,page}', {
+                owner: 'nihalashetty',
+                repo: 'gainsight-assessment'
+            });
 
-        setCommits(response.data)
+            setInvalidKey(false);
+            setCommits(response.data);
+            setIsTimerActive(true);
+        } catch {
+            setInvalidKey(true);
+        }
     }
 
     const commitElement = commits.map((item, index) => {
@@ -85,16 +110,16 @@ export default function GitCommits() {
             </button>
             :
             <button 
-                className="form--button"
+                className='form--button'
                 onClick={submitHandle}
+                disabled={key ? '' : 'disabled'}
             >
                 Submit
             </button>
             }
             
-            
             <div className="git-commits">
-                {commitElement}
+                {isTimerActive ? commitElement : invalidKey ? <p className='error'>Invalid Key</p> : <p>Enter Private Key and Submit to get the git commits</p>}
             </div>
         </main>
     )
