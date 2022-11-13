601c9a991c2a89a941c985c0d6e3f578a1ec469a
nihalashetty <nihalsetty48@gmail.com>
2022-11-13 21:46:18 +0530

    Adding timer to auto Refresh or click to Refresh

--- src/components/GitCommits.js
+++ src/components/GitCommits.js
@@ -5,19 +5,57 @@ import { Octokit } from "octokit"
 export default function GitCommits() {
     const [key, setKey] = React.useState('');
     const [commits, setCommits] = React.useState([]);
+    const [time, setTime] = React.useState(30);
+    const [isTimerActive, setIsTimerActive] = React.useState(false);
+
+    React.useEffect(() => {
+        let interval = null;
+        if (isTimerActive) {
+            interval = setInterval(() => {
+                console.log(time)
+                setTime((time) => {
+                    if (time === 0) {
+                        getCommits();
+                        return 30;
+                    } else {
+                        return time - 1;
+                    }
+                });
+            }, 1000);
+        } else {
+            clearInterval(interval);
+        }
+
+        return () => {
+            clearInterval(interval);
+        };
+    }, [isTimerActive]);
+
+    const changeHandle = (e) => {
+        setKey(e.target.value);
+        setIsTimerActive(false);
+    }
+
+    const submitHandle = () => {
+        getCommits();
+        setIsTimerActive(true);
+    }
+
+    const refreshHandle = () => {
+        setTime(0);
+        getCommits();
+    }
 
     const getCommits = async () => {
         const octokit = new Octokit({
             auth: `${key}`
         });
 
-
         const response = await octokit.request('GET /repos/{owner}/{repo}/commits{?sha,path,author,since,until,per_page,page}', {
             owner: 'nihalashetty',
             repo: 'gainsight-assessment'
         });
 
-        console.log(response)
         setCommits(response.data)
     }
 
@@ -30,21 +68,31 @@ export default function GitCommits() {
     
     return (
         <main>
-            <div></div>
             <input 
                 type="text"
                 placeholder="Enter Private Authentication Key"
                 className="form--input"
                 name="gitkey"
                 value={key}
-                onChange={e => setKey(e.target.value)}
+                onChange={e => changeHandle(e)}
             />
+            {isTimerActive ? 
+            <button 
+            className="form--button"
+            onClick={refreshHandle}
+            >
+                Auto Refresh in {time}s or Click to Refresh
+            </button>
+            :
             <button 
                 className="form--button"
-                onClick={getCommits}
+                onClick={submitHandle}
             >
                 Submit
             </button>
+            }
+            
+            
             <div className="git-commits">
                 {commitElement}
             </div>
